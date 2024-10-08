0e4ae626b493b48c42764ac2693a793d6b8bb725
Verdio Wong <verdio.wong.2023@scis.smu.edu.sg>
2024-10-08 19:58:48 +0800

    fixed build with maven

--- backend/src/test/java/com/example/profile/ProfileManagementApplicationTests.java
+++ backend/src/test/java/com/example/profile/ProfileManagementApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.profile;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.example.speed_climber.SpeedClimberApplication.class)
 class ProfileManagementApplicationTests {
 
 	@Test
