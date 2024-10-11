ba2d2201b2b2cd2fc6014f70ade5cfde120c22a4
rufaidah-ad <rufaidahadn@gmail.com>
2024-10-11 14:18:05 +0800

    Rename and move main to example folder

--- backend/src/test/java/com/example/profile/ProfileManagementApplicationTests.java
+++ backend/src/test/java/com/example/profile/ProfileManagementApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.profile;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = com.example.speed_climber.SpeedClimberApplication.class)
+@SpringBootTest(classes = com.example.Main.class)
 class ProfileManagementApplicationTests {
 
 	@Test
